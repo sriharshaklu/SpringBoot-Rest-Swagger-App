2e431dff67e9f4f8c58549e404674f740cd18db2
sriharshaklu <57980261+sriharshaklu@users.noreply.github.com>
2023-04-05 16:06:38 +0530

    Update WelcomeRestController.java

--- src/main/java/com/example/demo/resources/WelcomeRestController.java
+++ src/main/java/com/example/demo/resources/WelcomeRestController.java
@@ -14,7 +14,7 @@ public class WelcomeRestController {
 	@ApiOperation(value = "Get Welcome Message For The Given Name ", response = String.class, tags = "getWelcomeNote")
 	@GetMapping("/welcome/{name}")
 	public String welcome(@PathVariable("name") String name) {
-		return name+", Continuous Integration,Configuration management,Continuous delivery & devops..!!";
+		return name+", JENKINS AND DOCKER AND NAGIOS ..!!";
 	}
 
 }
